26e30a62dd0d14b7c990ac4f6fa60112d8706a73
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-03-28 19:36:04 -0500

    references and home page

--- Gemfile
+++ Gemfile
@@ -50,7 +50,7 @@ gem "bootsnap", require: false
 # use simple form gem for simple markup
 gem "simple_form", "~> 5.1.0"
 
-gem "devise", "~> 4.8.1"
+gem "devise"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_28_230657) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_29_001841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,9 +21,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_28_230657) do
   end
 
   create_table "quotes", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_quotes_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,8 +36,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_28_230657) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "quotes", "companies"
+  add_foreign_key "users", "companies"
 end
